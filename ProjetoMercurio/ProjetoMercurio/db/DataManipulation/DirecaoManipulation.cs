1f496f69780919d5edeb4ec538b15c941dcd9410
Leonardo Ribeiro Solano <61062447+lrsolano@users.noreply.github.com>
2021-05-11 23:37:53 -0300

    Manipulations

---
+++ ProjetoMercurio/ProjetoMercurio/db/DataManipulation/DirecaoManipulation.cs
@@ -0,0 +1,84 @@
+﻿using MercurioCore.db;
+using MercurioCore.Model.Exceptions;
+using MercurioCore.Model.Interfaces;
+using MySqlConnector;
+using ProjetoMercurio.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoMercurio.db.DataManipulation
+{
+    class DirecaoManipulation<T> : IRepository<T> where T : Direcao
+    {
+        private DBConnection connection;
+
+        public DirecaoManipulation()
+        {
+            connection = new DBConnection();
+        }
+        public T Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> FindAll()
+        {
+            List<T> items = new List<T>();
+            string sql = string.Format("SELECT IdDirecao, Movimento FROM  projetomercurio.direcao");
+            MySqlDataReader result = connection.SendQuery(sql);
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Direcao item = new Direcao((int)result["IdDirecao"], result["Movimento"].ToString());
+                    items.Add((T)item);
+                }
+                result.Close();
+
+                return (items);
+            }
+            else
+            {
+                throw new DBConnectionException("Nenhum registro encontrado");
+            }
+        }
+
+        public T FindByID(long id)
+        {
+            string sql = string.Format("SELECT IdDirecao, Movimento FROM  projetomercurio.direcao WHERE IdItem={0} ", id);
+            MySqlDataReader result = connection.SendQuery(sql);
+            if (result.HasRows)
+            {
+                result.Read();
+                Direcao item = new Direcao((int)result["IdDirecao"], result["Movimento"].ToString());
+                result.Close();
+                return (T)item;
+            }
+            else
+            {
+                throw new DBConnectionException("Nenhum registro encontrado");
+            }
+        }
+
+        public T FindLastId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjetoMercurio/ProjetoMercurio/db/DataManipulation/SensorManipulation.cs
@@ -0,0 +1,80 @@
+﻿using MercurioCore.db;
+using MercurioCore.Model.Exceptions;
+using MercurioCore.Model.Interfaces;
+using MySqlConnector;
+using ProjetoMercurio.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoMercurio.db.DataManipulation
+{
+    class SensorManipulation<T> : IRepository<T> where T : Sensor
+    {
+        private DBConnection connection;
+
+        public SensorManipulation()
+        {
+            connection = new DBConnection();
+        }
+        public T Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindByID(long id)
+        {
+            string sql = string.Format("SELECT IdSensor, Nome, DataCriacao, Inicial, IdSensorAnterior, IdDirecao, DirecaoRota FROM  projetomercurio.sensor WHERE IdSensor={0} ", id);
+            MySqlDataReader result = connection.SendQuery(sql);
+            /*if (result.HasRows)
+            {
+                Sensor sensor;
+                int idAnterior = 0;
+                if(!Int32.TryParse(result["IdSensorAnterior"].ToString(),out idAnterior))
+                {
+                    sensor = null;
+                }
+                else
+                {
+                    sensor = FindByID((idAnterior));
+                }
+
+                Direcao direcao = new Direcao((int)result["IdDirecao"]);
+                result.Read();
+                Sensor item = new Sensor((int)result["IdSensor"], result["Nome"].ToString(), (DateTime)result["DataCriacao"], (bool)result["Inicial"],sensor,direcao,);
+                result.Close();
+                return (T)item;
+            }
+            else
+            {
+                throw new DBConnectionException("Nenhum registro encontrado");
+            }*/
+            throw new NotImplementedException();
+        }
+
+        public T FindLastId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
